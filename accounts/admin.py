1f0b0afd4eb41435b1b3b21034b8eab0f06c7f8e
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-15 15:32:08 +0330

    complate model user and admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Users, Notification
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(Users)
+class UserAdmin(UserAdmin):
+    ...
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'body', 'created_at', 'is_active')
+    list_filter = ('is_active','created_at')
+    list_editable = ('is_active',)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Users(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    mobile_phone = models.CharField(_('mobile'), max_length=11, unique=True)
+    gender = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    gender = models.CharField(_('Gender'), max_length=6, choices=gender, default='Male')
+    
+    def __str__(self) -> str:
+        return self.username
+    
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        db_table = 'user'
+        
+
+class Notification(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='notifications')
+    body = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body
+    
+    
+    class Meta:
+        verbose_name = 'notification'
+        verbose_name_plural = 'notifications'
+        db_table = 'notifications'
