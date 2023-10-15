523a5d694e6a30bbe055bb7656ddd0552ad4dff5
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-15 15:41:37 +0330

    write model posts in app post

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ('user', 'is_active', 'created_at', 'updated_at')
+    list_editable = ('is_active', )
+    
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
+from django.utils import timezone
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from accounts.models import Users
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(upload_to='post/image',)
+    video = models.FileField(upload_to='post/video')
+    body = models.TextField(help_text='write any thing')
+    slug = models.SlugField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated post'), default=timezone.now)
+    
+    def __str__(self) -> str:
+        return self.user
+    
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'post'
+    
